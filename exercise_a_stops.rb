024bc298727bb8a03c3a76401fabc583c6c6881d
Oscar Tsang <ohhtsang@gmail.com>
2020-03-11 19:08:53 +0000

    Corrected question #4 to show index number from given value, also removed Linlithgow from array

--- exercise_a_stops.rb
+++ exercise_a_stops.rb
@@ -7,8 +7,10 @@ stops.unshift("Glasgow Queen St")
 #3. Add "Polmont" at the appropriate point (between "Falkirk High" and "Linlithgow")
 stops.insert(4, "Polmont")
 #4. Print out the index position of "Linlithgow"
-puts stops[5]
+puts stops.find_index("Linlithgow")
 #5. Remove "Livingston" from the array using its name
+stops.delete("Linlithgow")
+puts stops
 #6. Delete "Cumbernauld" from the array by index
 #7. Print the number of stops there are in the array?
 #8. Show as many ways as you can to return "Falkirk High" from the array?
