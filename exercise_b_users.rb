da400931e97ba05786bfe79752c6758ec8ac6b86
Oscar Tsang <ohhtsang@gmail.com>
2020-03-11 20:05:10 +0000

    retreieved smallest lottery number from Erik's hash

--- exercise_b_users.rb
+++ exercise_b_users.rb
@@ -61,7 +61,9 @@ puts users["Erik"][:home_town]
 # 3. Get the array of Erik's lottery numbers
 puts users["Erik"][:lottery_numbers]
 # 4. Get the species of Avril's pet Monty
+# puts users["Avril"][:pets]
 # 5. Get the smallest of Erik's lottery numbers
+puts users["Erik"][:lottery_numbers].min
 # 6. Return an array of Avril's lottery numbers that are even
 # 7. Erik is one lottery number short! Add the number `7` to be included in his lottery numbers
 # 8. Change Erik's hometown to Edinburgh
