dd4719067d82cfef496266c5fea085f236269106
Oscar Tsang <ohhtsang@gmail.com>
2020-03-11 19:46:17 +0000

    retrieved Erik's home_town from hash, retrieved array of Erik's lottery numbers from hash

--- exercise_b_users.rb
+++ exercise_b_users.rb
@@ -57,7 +57,9 @@ users = {
 # 1. Get Jonathan's Twitter handle (i.e. the string `"jonnyt"`)
 puts users["Jonathan"][:twitter]
 # 2. Get Erik's hometown
+puts users["Erik"][:home_town]
 # 3. Get the array of Erik's lottery numbers
+puts users["Erik"][:lottery_numbers]
 # 4. Get the species of Avril's pet Monty
 # 5. Get the smallest of Erik's lottery numbers
 # 6. Return an array of Avril's lottery numbers that are even
