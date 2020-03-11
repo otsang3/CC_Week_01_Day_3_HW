4ae0035b90cb514673182537c39c421313607280
Oscar Tsang <ohhtsang@gmail.com>
2020-03-11 22:04:22 +0000

    added loop functions for q3 and q4

--- exercise_c_uk.rb
+++ exercise_c_uk.rb
@@ -21,6 +21,24 @@ united_kingdom[1][:capital] = "Cardiff"
 # puts united_kingdom
 # 2. Create a Hash for Northern Ireland and add it to the `united_kingdom` array (The capital is Belfast, and the population is 1,811,000).
 united_kingdom << {:name => "Northern Ireland", :population => 1_811_000, :capital => "Belfast"}
-puts united_kingdom
+# puts united_kingdom
 # 3. Use a loop to print the names of all the countries in the UK.
+def countries_name(array)
+  for country_name in array
+    puts country_name[:name]
+  end
+end
+
+countries_name(united_kingdom)
+
 # 4. Use a loop to find the total population of the UK.
+def add_population(array_of_countries)
+  total_population = 0
+  for population in array_of_countries
+    total_population += population[:population]
+  end
+  return total_population
+end
+
+pop = add_population(united_kingdom)
+puts pop
